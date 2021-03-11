707dd73520b779146c2640cde20ba71ded667076
aatma001 <70138225+aatma001@users.noreply.github.com>
2021-03-11 23:53:04 +0200

    lights for electric motorcycle

---
+++ controls.js
@@ -0,0 +1,19 @@
+function controlHeadlights(headlightState) {
+  console.log(headlightState)
+}
+
+function controlLeftBlink(leftBlinkState) {
+  console.log(leftBlinkState)
+}
+
+function controlRightBlink(rightBlinkState) {
+  console.log(rightBlinkState)
+}
+
+function controlHorn(hornState) {
+  console.log(hornState)
+}
+
+module.exports = {
+  controlHeadlights, controlHorn, controlLeftBlink, controlRightBlink
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const controls = require("./controls")
+
+io.on("connection", (socket) => {
+  console.log('a user connected');
+
+  socket.on("headlights", (headlightState) => {
+    controls.controlHeadlights(headlightState)
+  })
+
+  socket.on("blinkleft", (leftBlinkState) => {
+    controls.controlLeftBlink(leftBlinkState)
+  })
+
+  socket.on("blinkright", (rightBlinkState) => {
+    controls.controlRightBlink(rightBlinkState)
+  })
+
+  socket.on("horn", (hornState) => {
+    controls.controlHorn(hornState)
+  })
+});
+app.use(express.static('../frontend/build'))
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
